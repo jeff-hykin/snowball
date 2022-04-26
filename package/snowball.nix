eade429eb4c33da66db44578cfa146e5f17a8dfe
Jeff Hykin <jeff.hykin@gmail.com>
2022-04-26 11:53:24 -0500

    =

--- package/snowball.nix
+++ package/snowball.nix
@@ -8,68 +8,104 @@
             )
             ({})
         );
+        variant  = "default";
+        lib      = nixpkgs.lib;
+        stdenv   = nixpkgs.stdenv;
+        isPy37   = nixpkgs.isPy37;
+        isPy38   = nixpkgs.isPy38;
+        isPy39   = nixpkgs.isPy39;
+        isPy310  = nixpkgs.isPy310;
+        patchelf = nixpkgs.patchelf;
+        pillow   = nixpkgs.pillow;
+        python   = nixpkgs.python;
+        pytorch  = nixpkgs.pytorch;
+        buildPythonPackage = nixpkgs.buildPythonPackage;
     };
-    outputs = { meta, inputs, ... }:
+    outputs = { variant, lib, stdenv, isPy37, isPy38, isPy39, isPy310, patchelf, pillow, python, pytorch, buildPythonPackage, ... }:
         let 
-            lib                = inputs.lib;
-            stdenv             = inputs.stdenv;
-            buildPythonPackage = inputs.buildPythonPackage;
-            fetchurl           = inputs.fetchurl;
-            isPy37             = inputs.isPy37;
-            isPy38             = inputs.isPy38;
-            isPy39             = inputs.isPy39;
-            isPy310            = inputs.isPy310;
-            patchelf           = inputs.patchelf;
-            pillow             = inputs.pillow;
-            python             = inputs.python;
-            pytorch            = inputs.pytorch;
-            rpath              = lib.makeLibraryPath [ stdenv.cc.cc.lib ];
-            version            = meta.version;
-            date               = meta.date;
-            pyVerNoDot         = builtins.replaceStrings [ "." ] [ "" ] python.pythonVersion;
-            srcs        = import (builtins.fetchurl (meta.nightly_binary_hashes)) { inherit version date; };
+            info = rec {
+                name = "torchvision";
+                version = "0.13.0";
+                description = "PyTorch vision library";
+                homepage = "https://pytorch.org/";
+                changelog = "https://github.com/pytorch/vision/releases/tag/v${version}";
+                date = "20220420";
+                license = {
+                    deprecated = false;
+                    free = true;
+                    fullName = "BSD 3-clause \"New\" or \"Revised\" License";
+                    redistributable = true;
+                    shortName = "bsd3";
+                    spdxId = "BSD-3-Clause";
+                    url = "https://spdx.org/licenses/BSD-3-Clause.html";
+                }; # see lib.licenses to see whats available
+                maintainers = [];
+                platforms = [
+                    [ "x86_64-linux" ]
+                    # "aarch64-linux"
+                    # "armv7a-linux"
+                    # "armv7l-linux"
+                    
+                    # print lib.platforms.linux to see whats available
+                ];
+                nightly_binary_hashes = "https://raw.githubusercontent.com/rehno-lindeque/ml-pkgs/master/pkgs/torchvision/nightly-binary-hashes.nix";
+            };
+            srcs = (builtins.import
+                (builtins.fetchurl (info.nightly_binary_hashes))
+                ({
+                    version = info.version;
+                    date = info.date;
+                })
+            );
+            rpath       = lib.makeLibraryPath [ stdenv.cc.cc.lib ];
+            pyVerNoDot  = builtins.replaceStrings [ "." ] [ "" ] python.pythonVersion;
             unsupported = throw "Unsupported system";
-        in
-            rec {
-                package0 = inputs.buildPythonPackage  {
-                    version = "${version}-${date}";
-                    pname = meta.name;
-                    format = "wheel";
-                    src = fetchurl srcs."${stdenv.system}-${pyVerNoDot}" or unsupported;
-                    disabled = !(isPy37 || isPy38 || isPy39 || isPy310);
-                    nativeBuildInputs = [
-                        patchelf
-                    ];
-                    propagatedBuildInputs = [
-                        pillow
-                        pytorch
-                    ];
+            
+            package0 = buildPythonPackage  {
+                version = "${info.version}-${info.date}";
+                pname = info.name;
+                format = "wheel";
+                
+                disabled = !(isPy37 || isPy38 || isPy39 || isPy310);
+                src = (builtins.fetchurl (srcs."${stdenv.system}-${pyVerNoDot}") || unsupported);
+                
+                nativeBuildInputs = [
+                    patchelf
+                ];
+                propagatedBuildInputs = [
+                    pillow
+                    pytorch
+                ];
 
-                    # The wheel-binary is not stripped to avoid the error of `ImportError: libtorch_cuda_cpp.so: ELF load command address/offset not properly aligned.`.
-                    dontStrip = true;
+                # The wheel-binary is not stripped to avoid the error of `ImportError: libtorch_cuda_cpp.so: ELF load command address/offset not properly aligned.`.
+                dontStrip = true;
 
-                    pythonImportsCheck = [ "torchvision" ];
+                pythonImportsCheck = [ "torchvision" ];
 
-                    postFixup = ''
-                        # Note: after patchelf'ing, libcudart can still not be found. However, this should
-                        #       not be an issue, because PyTorch is loaded before torchvision and brings
-                        #       in the necessary symbols.
-                        patchelf --set-rpath "${rpath}:${pytorch}/${python.sitePackages}/torch/lib:" \
-                        "$out/${python.sitePackages}/torchvision/_C.so"
-                    '';
+                postFixup = ''
+                    # Note: after patchelf'ing, libcudart can still not be found. However, this should
+                    #       not be an issue, because PyTorch is loaded before torchvision and brings
+                    #       in the necessary symbols.
+                    patchelf --set-rpath "${rpath}:${pytorch}/${python.sitePackages}/torch/lib:" \
+                    "$out/${python.sitePackages}/torchvision/_C.so"
+                '';
 
-                    meta = with lib; {
-                        description = "PyTorch vision library";
-                        homepage = "https://pytorch.org/";
-                        changelog = "https://github.com/pytorch/vision/releases/tag/v${version}";
-                        # Includes CUDA and Intel MKL, but redistributions of the binary are not limited.
-                        # https://docs.nvidia.com/cuda/eula/index.html
-                        # https://www.intel.com/content/www/us/en/developer/articles/license/onemkl-license-faq.html
-                        license = licenses.bsd3;
-                        platforms = platforms.linux;
-                        maintainers = with maintainers; [];
-                    };
+                meta = with lib; {
+                    description = info.description;
+                    homepage = info.homepage;
+                    changelog = info.changelog;
+                    # Includes CUDA and Intel MKL, but redistributions of the binary are not limited.
+                    # https://docs.nvidia.com/cuda/eula/index.html
+                    # https://www.intel.com/content/www/us/en/developer/articles/license/onemkl-license-faq.html
+                    license = info.license;
+                    platforms = info.platforms;
+                    maintainers = with maintainers; [];
                 };
+            };
+        in
+            {
+                info = info;
+                package0 = package0;
                 nixShell = {
                     buildInputs = [ package0 ];
                 };
