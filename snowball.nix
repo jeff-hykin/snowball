0250dce6db0aa76b739d21374ffbcd52477b9065
Jeff Hykin <jeff.hykin@gmail.com>
2022-04-25 22:47:34 -0500

    -

--- snowball.nix
+++ snowball.nix
@@ -1,6 +1,6 @@
 url : 
     let 
-        meta = (builtins.fromTOML (builtins.fetchurl "${url}/package/info.toml"));
+        meta = (builtins.fromTOML (builtins.readFile (builtins.fetchurl "${url}/package/info.toml"))).meta;
         snowball = (builtins.import (builtins.fetchurl "${url}/package/snowball.nix"));
         outputs = (snowball.outputs 
             ({
