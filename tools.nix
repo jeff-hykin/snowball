e59b28e8eadd8112e40c90e88e926fe5b5c3206f
Jeff Hykin <jeff.hykin@gmail.com>
2022-05-16 12:13:58 -0500

    -

--- tools.nix
+++ tools.nix
@@ -1,5 +1,5 @@
 # tools = import (builtins.fetchurl "https://raw.githubusercontent.com/jeff-hykin/snowball/5e568e55e44819606cffa5ad25a3b26a14fee11a/tools.nix")
-{
+rec {
     # a function for cutting down on boilerplate
     nixpkgsAt =  commitHash : 
         (builtins.import
@@ -11,4 +11,18 @@
             ({})
         )
     ;
+    getSnowball = { url, inputs, ... } : 
+        let 
+            snow = (builtins.import (builtins.fetchurl url));
+            outputs = (snow.outputs 
+                ({
+                    inputs = snow.inputs // inputs;
+                })
+            );
+        in
+            outputs
+    ;
+    snowball = {
+        values = args: (getSnowball (args)).values;
+    };
 }
