3e62aaf5515b314a6e440e327055931d90716a58
Jeff Hykin <jeff.hykin@gmail.com>
2022-05-21 11:00:54 -0500

    setup ozone

--- package/snowball.nix
+++ package/snowball.nix
@@ -8,98 +8,117 @@
             )
             ({})
         );
-        variant  = "default";
-        lib      = nixpkgs.lib;
-        stdenv   = nixpkgs.stdenv;
-        isPy37   = nixpkgs.isPy37;
-        isPy38   = nixpkgs.isPy38;
-        isPy39   = nixpkgs.isPy39;
-        isPy310  = nixpkgs.isPy310;
-        patchelf = nixpkgs.patchelf;
-        pillow   = nixpkgs.pillow;
-        python   = nixpkgs.python;
-        pytorch  = nixpkgs.pytorch;
-        buildPythonPackage = nixpkgs.buildPythonPackage;
+        variant       = "default";
+        lib           = nixpkgs.lib;
+        stdenv        = nixpkgs.stdenv;
+        fetchurl      = nixpkgs.fetchurl;
+        fontconfig    = nixpkgs.fontconfig;
+        freetype      = nixpkgs.freetype;
+        libICE        = nixpkgs.libICE;
+        libSM         = nixpkgs.libSM;
+        udev          = nixpkgs.udev;
+        libX11        = nixpkgs.libX11;
+        libXcursor    = nixpkgs.libXcursor;
+        libXext       = nixpkgs.libXext;
+        libXfixes     = nixpkgs.libXfixes;
+        libXrandr     = nixpkgs.libXrandr;
+        libXrender    = nixpkgs.libXrender;
     };
-    outputs = { variant, lib, stdenv, isPy37, isPy38, isPy39, isPy310, patchelf, pillow, python, pytorch, buildPythonPackage, ... }:
+    outputs = { nixpkgs, variant, lib, stdenv, fetchurl, fontconfig, freetype, libICE, libSM, udev, libX11, libXcursor, libXext, libXfixes, libXrandr, libXrender, ... }:
         let 
-            info = rec {
-                name = "torchvision";
-                version = "0.13.0";
-                description = "PyTorch vision library";
-                homepage = "https://pytorch.org/";
-                changelog = "https://github.com/pytorch/vision/releases/tag/v${version}";
-                date = "20220420";
-                license = {
-                    deprecated = false;
-                    free = true;
-                    fullName = "BSD 3-clause \"New\" or \"Revised\" License";
-                    redistributable = true;
-                    shortName = "bsd3";
-                    spdxId = "BSD-3-Clause";
-                    url = "https://spdx.org/licenses/BSD-3-Clause.html";
-                }; # see lib.licenses to see whats available
-                maintainers = [];
-                platforms = [
-                    [ "x86_64-linux" ]
-                    # "aarch64-linux"
-                    # "armv7a-linux"
-                    # "armv7l-linux"
+            # info = rec {
+            #     name = "torchvision";
+            #     version = "0.13.0";
+            #     description = "PyTorch vision library";
+            #     homepage = "https://pytorch.org/";
+            #     changelog = "https://github.com/pytorch/vision/releases/tag/v${version}";
+            #     date = "20220420";
+            #     license = {
+            #         deprecated = false;
+            #         free = true;
+            #         fullName = "BSD 3-clause \"New\" or \"Revised\" License";
+            #         redistributable = true;
+            #         shortName = "bsd3";
+            #         spdxId = "BSD-3-Clause";
+            #         url = "https://spdx.org/licenses/BSD-3-Clause.html";
+            #     }; # see lib.licenses to see whats available
+            #     maintainers = [];
+            #     platforms = [
+            #         [ "x86_64-linux" ]
+            #         # "aarch64-linux"
+            #         # "armv7a-linux"
+            #         # "armv7l-linux"
                     
-                    # print lib.platforms.linux to see whats available
-                ];
-                nightly_binary_hashes = "https://raw.githubusercontent.com/rehno-lindeque/ml-pkgs/master/pkgs/torchvision/nightly-binary-hashes.nix";
-            };
-            srcs = (builtins.import
-                (builtins.fetchurl (info.nightly_binary_hashes))
-                ({
-                    version = info.version;
-                    date = info.date;
-                })
-            );
-            rpath       = lib.makeLibraryPath [ stdenv.cc.cc.lib ];
-            pyVerNoDot  = builtins.replaceStrings [ "." ] [ "" ] python.pythonVersion;
-            unsupported = throw "Unsupported system";
-            
-            package0 = buildPythonPackage  {
-                version = "${info.version}-${info.date}";
-                pname = info.name;
-                format = "wheel";
-                
-                disabled = !(isPy37 || isPy38 || isPy39 || isPy310);
-                src = (builtins.fetchurl (srcs."${stdenv.system}-${pyVerNoDot}") || unsupported);
-                
-                nativeBuildInputs = [
-                    patchelf
-                ];
-                propagatedBuildInputs = [
-                    pillow
-                    pytorch
-                ];
+            #         # print lib.platforms.linux to see whats available
+            #     ];
+            #     nightly_binary_hashes = "https://raw.githubusercontent.com/rehno-lindeque/ml-pkgs/master/pkgs/torchvision/nightly-binary-hashes.nix";
+            # };
+            info = {};
+            package0 = stdenv.mkDerivation rec {
+                pname = "segger-ozone";
+                version = "3.22a";
 
-                # The wheel-binary is not stripped to avoid the error of `ImportError: libtorch_cuda_cpp.so: ELF load command address/offset not properly aligned.`.
-                dontStrip = true;
+                src = fetchurl {
+                    url = "https://www.segger.com/downloads/jlink/Ozone_Linux_V${(lib.replaceChars ["."] [""] version)}_x86_64.tgz";
+                    sha256 = "0v1r8qvp1w2f3yip9fys004pa0smlmq69p7w77lfvghs1rmg1649";
+                };
 
-                pythonImportsCheck = [ "torchvision" ];
+                rpath = lib.makeLibraryPath [
+                    fontconfig
+                    freetype
+                    libICE
+                    libSM
+                    udev
+                    libX11
+                    libXcursor
+                    libXext
+                    libXfixes
+                    libXrandr
+                    libXrender
+                ] + ":${stdenv.cc.cc.lib}/lib64";
+
+                installPhase = ''
+                    mkdir -p $out/bin
+                    mv Lib lib
+                    mv * $out
+                    ln -s $out/Ozone $out/bin
+                '';
 
                 postFixup = ''
-                    # Note: after patchelf'ing, libcudart can still not be found. However, this should
-                    #       not be an issue, because PyTorch is loaded before torchvision and brings
-                    #       in the necessary symbols.
-                    patchelf --set-rpath "${rpath}:${pytorch}/${python.sitePackages}/torch/lib:" \
-                    "$out/${python.sitePackages}/torchvision/_C.so"
+                    udev --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$out/Ozone" \
+                    --set-rpath ${rpath}:$out/lib "$out/Ozone"
+
+                    for file in $(find $out/lib -maxdepth 1 -type f -and -name \*.so\*); do
+                        udev --set-rpath ${rpath}:$out/lib $file
+                    done
                 '';
 
                 meta = with lib; {
-                    description = info.description;
-                    homepage = info.homepage;
-                    changelog = info.changelog;
-                    # Includes CUDA and Intel MKL, but redistributions of the binary are not limited.
-                    # https://docs.nvidia.com/cuda/eula/index.html
-                    # https://www.intel.com/content/www/us/en/developer/articles/license/onemkl-license-faq.html
-                    license = info.license;
-                    platforms = info.platforms;
-                    maintainers = with maintainers; [];
+                    description = "J-Link Debugger and Performance Analyzer";
+                    longDescription = ''
+                        Ozone is a cross-platform debugger and performance analyzer for J-Link
+                        and J-Trace.
+
+                            - Stand-alone graphical debugger
+                            - Debug output of any tool chain and IDE 1
+                            - C/C++ source level debugging and assembly instruction debugging
+                            - Debug information windows for any purpose: disassembly, memory,
+                            globals and locals, (live) watches, CPU and peripheral registers
+                            - Source editor to fix bugs immediately
+                            - High-speed programming of the application into the target
+                            - Direct use of J-Link built-in features (Unlimited Flash
+                            Breakpoints, Flash Download, Real Time Terminal, Instruction Trace)
+                            - Scriptable project files to set up everything automatically
+                            - New project wizard to ease the basic configuration of new projects
+
+                        1 Ozone has been tested with the output of the following compilers:
+                        GCC, Clang, ARM, IAR. Output of other compilers may be supported but is
+                        not guaranteed to be.
+                    '';
+                    homepage = "https://www.segger.com/products/development-tools/ozone-j-link-debugger";
+                    license = licenses.unfree;
+                    maintainers = [ maintainers.bmilanov ];
+                    platforms = [ "x86_64-linux" ];
                 };
             };
         in
