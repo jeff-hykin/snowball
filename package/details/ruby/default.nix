c4c98f93ff9de3629d35283effa4ae5751d29062
Jeff Hykin <jeff.hykin@gmail.com>
2022-05-25 12:56:32 -0500

    remove config.nix

--- package/details/ruby/default.nix
+++ package/details/ruby/default.nix
@@ -11,7 +11,12 @@ let
   ops = lib.optionals;
   opString = lib.optionalString;
   patchSet = import ./rvm-patchsets.nix { inherit fetchFromGitHub; };
-  config = import ./config.nix { inherit fetchFromSavannah; };
+  # Ruby >= 2.1.0 tries to download config.{guess,sub}
+  config = fetchFromSavannah {
+        repo = "config";
+        rev = "576c839acca0e082e536fd27568b90a446ce5b96";
+        sha256 = "11bjngchjhj0qq0ppp8c37rfw0yhp230nvhs2jvlx15i9qbf56a0";
+    };
   rubygems = import ./rubygems { inherit stdenv lib fetchurl; };
 
   # Contains the ruby version heuristics
