08a210771581a130f7f4b613aab59ce8270a10b9
Jeff Hykin <jeff.hykin@gmail.com>
2022-05-25 14:56:38 -0500

    create snowball pack

--- tools.nix
+++ tools.nix
@@ -11,6 +11,37 @@ rec {
             ({})
         )
     ;
+    packSnowball = { inputs, outputs } : 
+        let
+            evaledInputs = (builtins.foldl'
+                # with each input add the key-value pair to a set, and evaluate any inputs that are functions, giving them the already-evaled inputs as inputs
+                (accumulator:
+                    {key, value}:
+                        accumulator // {
+                            "${key}" =
+                                if (builtins.isFunction (value))
+                                then value (accumulator)
+                                else value
+                            ;
+                        }
+                )
+                # start with an empty {}
+                ({ })
+                # create a list of {key=, value=}
+                (builtins.map
+                    (each: 
+                        {
+                            key = each;
+                            value = (builtins.getAttr (each) inputs);
+                        }
+                    )
+                    (builtins.attrNames inputs)
+                )
+            );
+            outputs = (snow.outputs (evaledInputs));
+        in
+            outputs
+    ;
     getSnowball = { url, inputs, ... } : 
         let 
             snow = (builtins.import (builtins.fetchurl url));
