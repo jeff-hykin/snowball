cbaed392fe1fa9e8b0bff5cfdafdde4acaa11fd7
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-10 18:11:59 -0500

    2.7.18

--- snowball/snow.nix
+++ snowball/snow.nix
@@ -16,11 +16,11 @@
                     # packages."x86_64-linux" = pkgs.stdenv.mkDerivation {
                     # 
                     # };
-                    packages."x86_64-linux"   = pkgs.python;
-                    packages."aarch64-linux"  = pkgs.python;
-                    packages."i686-linux"     = pkgs.python;
-                    packages."x86_64-darwin"  = pkgs.python;
-                    packages."aarch64-darwin" = pkgs.python;
+                    packages."x86_64-linux"   = pkgs.python27Full;
+                    packages."aarch64-linux"  = pkgs.python27Full;
+                    packages."i686-linux"     = pkgs.python27Full;
+                    packages."x86_64-darwin"  = pkgs.python27Full;
+                    packages."aarch64-darwin" = pkgs.python27Full;
                 }
         ;
     }
