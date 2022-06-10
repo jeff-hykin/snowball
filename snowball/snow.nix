656e2f614d13301d15504fbcad55d2ef24469181
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-10 18:42:05 -0500

    3.10.2

--- snowball/snow.nix
+++ snowball/snow.nix
@@ -1,9 +1,9 @@
 {
-        # commit date: 2022-04-14
-        # probably can view at: https://github.com/NixOS/nixpkgs/blob/5c7148bd9fbd855f29ba33506724c38f7f0ec484/pkgs/development/interpreters/python/cpython/default.nix
+        # commit date: 2022-03-15
+        # probably can view at: https://github.com/NixOS/nixpkgs/blob/8d18f29b6001756116cbb188468ef10178f44046/pkgs/development/interpreters/python/cpython/default.nix
         inputs = {
-            nixpkgsHash   = { ...} : "5c7148bd9fbd855f29ba33506724c38f7f0ec484";
-            pkgs          = { nixpkgsHash, ...} : (builtins.import (builtins.fetchTarball ({url="https://github.com/NixOS/nixpkgs/archive/5c7148bd9fbd855f29ba33506724c38f7f0ec484.tar.gz";}) ) ({}) );
+            nixpkgsHash   = { ...} : "8d18f29b6001756116cbb188468ef10178f44046";
+            pkgs          = { nixpkgsHash, ...} : (builtins.import (builtins.fetchTarball ({url="https://github.com/NixOS/nixpkgs/archive/8d18f29b6001756116cbb188468ef10178f44046.tar.gz";}) ) ({}) );
             # customInput1 = { pkgs, ...} : pkgs.something;
             # customInput2 = { customInput1, ...} : customInput1.subPackage;
         };
@@ -16,11 +16,11 @@
                     # packages."x86_64-linux" = pkgs.stdenv.mkDerivation {
                     # 
                     # };
-                    packages."x86_64-linux"   = pkgs.sourcehut.python;
-                    packages."aarch64-linux"  = pkgs.sourcehut.python;
-                    packages."i686-linux"     = pkgs.sourcehut.python;
-                    packages."x86_64-darwin"  = pkgs.sourcehut.python;
-                    packages."aarch64-darwin" = pkgs.sourcehut.python;
+                    packages."x86_64-linux"   = pkgs.python310;
+                    packages."aarch64-linux"  = pkgs.python310;
+                    packages."i686-linux"     = pkgs.python310;
+                    packages."x86_64-darwin"  = pkgs.python310;
+                    packages."aarch64-darwin" = pkgs.python310;
                 }
         ;
     }
