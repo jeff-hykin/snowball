ad9864d597088b1c4ce1a2e213f8c0003c188ffc
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-10 18:50:34 -0500

    3.9.6

--- snowball/snow.nix
+++ snowball/snow.nix
@@ -16,11 +16,11 @@
                     # packages."x86_64-linux" = pkgs.stdenv.mkDerivation {
                     # 
                     # };
-                    packages."x86_64-linux"   = pkgs.gnuradio3_8Packages.python;
-                    packages."aarch64-linux"  = pkgs.gnuradio3_8Packages.python;
-                    packages."i686-linux"     = pkgs.gnuradio3_8Packages.python;
-                    packages."x86_64-darwin"  = pkgs.gnuradio3_8Packages.python;
-                    packages."aarch64-darwin" = pkgs.gnuradio3_8Packages.python;
+                    packages."x86_64-linux"   = pkgs.python3Full;
+                    packages."aarch64-linux"  = pkgs.python3Full;
+                    packages."i686-linux"     = pkgs.python3Full;
+                    packages."x86_64-darwin"  = pkgs.python3Full;
+                    packages."aarch64-darwin" = pkgs.python3Full;
                 }
         ;
     }
