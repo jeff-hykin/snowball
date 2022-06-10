4bfa705e073c44f21ec436288370881fc01e35b1
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-10 14:56:08 -0500

    WIP

---
+++ fornix/cli_tools/extract_to_repo.js
@@ -0,0 +1,155 @@
+#!/usr/bin/env -S deno run --allow-all
+import { getPackageInfo, allCommitsFor } from "../support/nixpkgs.js"
+import { jsonRead } from "../support/basics.js"
+
+const { run, Timeout, Env, Cwd, Stdin, Stdout, Stderr, Out, Overwrite, AppendTo, zipInto, mergeInto, returnAsString, } = await import(`https://deno.land/x/quickr@0.3.24/main/run.js`)
+const { FileSystem } = await import(`https://deno.land/x/quickr@0.3.24/main/file_system.js`)
+
+const name = Deno.args[0]
+
+// 
+// create a target repo
+// 
+const repoFolder = `${FileSystem.thisFolder}/../cache.ignore/extracted_repos/${name}`; await FileSystem.ensureIsFolder(repoFolder)
+const snowballPath = `${repoFolder}/snowball/snow.nix`
+await run`git clone https://github.com/jeff-hykin/snowball ${repoFolder}`
+await (FileSystem.cwd = repoFolder)
+// checkout the correct path
+const currentBranchName = await run`git rev-parse --abbrev-ref HEAD ${Stdout(returnAsString)}`
+const packageBranchName = `packages/${name}`
+await run`git switch ${currentBranchName}`
+await run`git checkout -b ${`packages/${name}`}`
+await run`git push --set-upstream origin ${`packages/${name}`}`
+
+const commits = await getReleventCommitsFor({packageName: name})
+for (const [hash, dateString] of Object.entries(commits)) {
+    const infos = await getPackageInfo({hash, packageName})
+    // {
+    //     "nixpkgs.python": {
+    //         "name": "python-2.7.18",
+    //         "pname": "python",
+    //         "version": "2.7.18",
+    //         "system": "x86_64-linux",
+    //         "meta": {
+    //         "available": true,
+    //         "description": "A high-level dynamically-typed programming language",
+    //         "homepage": "http://python.org",
+    //         "license": {
+    //             "fullName": "Python Software Foundation License version 2",
+    //             "shortName": "psfl",
+    //             "spdxId": "Python-2.0",
+    //             "url": "https://spdx.org/licenses/Python-2.0.html"
+    //         },
+    //         "longDescription": "Python is a remarkably powerful dynamic programming language that\nis used in a wide variety of application domains. Some of its key\ndistinguishing features include: clear, readable syntax; strong\nintrospection capabilities; intuitive object orientation; natural\nexpression of procedural code; full modularity, supporting\nhierarchical packages; exception-based error handling; and very\nhigh level dynamic data types.\n",
+    //         "maintainers": [
+    //             {
+    //             "email": "fridh@fridh.nl",
+    //             "github": "fridh",
+    //             "githubId": 2129135,
+    //             "name": "Frederik Rietdijk"
+    //             }
+    //         ],
+    //         "name": "python-2.7.18",
+    //         "outputsToInstall": [
+    //             "out"
+    //         ],
+    //         "platforms": [
+    //             "aarch64-linux",
+    //             "armv5tel-linux",
+    //             "armv6l-linux",
+    //             "armv7a-linux",
+    //             "armv7l-linux",
+    //             "mipsel-linux",
+    //             "i686-cygwin",
+    //             "i686-freebsd",
+    //             "i686-linux",
+    //             "i686-netbsd",
+    //             "i686-openbsd",
+    //             "x86_64-cygwin",
+    //             "x86_64-freebsd",
+    //             "x86_64-linux",
+    //             "x86_64-netbsd",
+    //             "x86_64-openbsd",
+    //             "x86_64-solaris",
+    //             "x86_64-darwin",
+    //             "i686-darwin",
+    //             "aarch64-darwin",
+    //             "armv7a-darwin",
+    //             "x86_64-windows",
+    //             "i686-windows",
+    //             "wasm64-wasi",
+    //             "wasm32-wasi",
+    //             "x86_64-redox",
+    //             "powerpc64le-linux",
+    //             "riscv32-linux",
+    //             "riscv64-linux",
+    //             "arm-none",
+    //             "armv6l-none",
+    //             "aarch64-none",
+    //             "avr-none",
+    //             "i686-none",
+    //             "x86_64-none",
+    //             "powerpc-none",
+    //             "msp430-none",
+    //             "riscv64-none",
+    //             "riscv32-none",
+    //             "vc4-none",
+    //             "js-ghcjs",
+    //             "aarch64-genode",
+    //             "i686-genode",
+    //             "x86_64-genode"
+    //         ],
+    //         "position": "/nix/store/x0q87hvyab6431g84iswgr92qz5wngaw-nixpkgs-20.09pre240426.f9567594d5a/nixpkgs/pkgs/development/interpreters/python/cpython/2.7/default.nix:278",
+    //         "priority": -100
+    //         }
+    //     }
+    // }            
+    for (const [attributePath, info] of Object.entries(infos)) {
+        if (info.version && info.meta && info.meta.path) {
+            const snowballString = generateSnowballString({
+                nixpkgsHash: hash,
+                attributePath,
+                relativePath: info.meta.path,
+            })
+            await FileSystem.write({ data: snowballString, path: snowballPath,})
+            await run`git add -A`
+            await run`git commit -m ${}`
+            git tag "$tag_name"
+            git push origin "$tag_name"
+        }
+    }
+}
+// - git log on that file path to get relvent commits
+// - iterate over the commits
+//      - extract version info, and attribute path
+//      - generate a snowball.nix
+//      - add/stage, commit changes, push,
+//      - tag with version
+
+function generateSnowballString({ nixpkgsHash, attributePath, relativePath }) {
+    return `{
+        # probably can view at: https://github.com/NixOS/nixpkgs/blob/${nixpkgsHash}/${relativePath}
+        inputs = {
+            nixpkgsHash   = { ...} : "${nixpkgsHash}";
+            pkgs          = { nixpkgsHash, ...} : (builtins.import (builtins.fetchTarball ({url="https://github.com/NixOS/nixpkgs/archive/${nixpkgsHash}.tar.gz";}) ) ({}) );
+            # customInput1 = { pkgs, ...} : pkgs.something;
+            # customInput2 = { customInput1, ...} : customInput1.subPackage;
+        };
+        outputs = { pkgs, ... }@input:
+            let
+                exampleValue1 = "blah";
+                exampleValue2 = "blah";
+            in
+                {
+                    # packages."x86_64-linux" = pkgs.stdenv.mkDerivation {
+                    # 
+                    # };
+                    packages."x86_64-linux"   = pkgs.${attributePath};
+                    packages."aarch64-linux"  = pkgs.${attributePath};
+                    packages."i686-linux"     = pkgs.${attributePath};
+                    packages."x86_64-darwin"  = pkgs.${attributePath};
+                    packages."aarch64-darwin" = pkgs.${attributePath};
+                }
+        ;
+    }`
+}
---
+++ fornix/support/nixpkgs.js
@@ -0,0 +1,110 @@
+#!/usr/bin/env -S deno run --allow-all
+
+const { run, Timeout, Env, Cwd, Stdin, Stdout, Stderr, Out, Overwrite, AppendTo, zipInto, mergeInto, returnAsString, } = await import(`https://deno.land/x/quickr@0.3.24/main/run.js`)
+const { FileSystem } = await import(`https://deno.land/x/quickr@0.3.24/main/file_system.js`)
+const { Console, yellow } = await import(`https://deno.land/x/quickr@0.3.24/main/console.js`)
+const { recursivelyAllKeysOf, get, set, remove, merge, compare } = await import(`https://deno.land/x/good@0.5.8/object.js`)
+const { jsonRead } = await import(`../support/basics.js`)
+
+const cacheFolder = `${FileSystem.thisFolder}/../cache.ignore/nixpkgs/jsons`; await FileSystem.ensureIsFolder(cacheFolder)
+const nixpkgsFolder = `${FileSystem.thisFolder}/../cache.ignore/nixpkgs/repo`
+const allCommitsPath = `${FileSystem.thisFolder}/../cache.ignore/nixpkgs/all_commits.txt`
+
+Console.env.NIXPKGS_ALLOW_BROKEN = "1"
+Console.env.NIXPKGS_ALLOW_UNFREE = "1"
+Console.env.NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM = "1"
+Console.env.NIX_PATH = ""
+Console.env.HOME = FileSystem.thisFolder
+
+// 
+// clone nixpkgs to the correct place
+// 
+await run`git clone https://github.com/NixOS/nixpkgs.git ${nixpkgsFolder}`
+const latestCommitHash = await run`git rev-parse HEAD ${Stdout(returnAsString)}`
+
+
+
+let allCommitsCache = null
+export async function allCommitsAndDates() {
+    if (allCommitsCache) {
+        return allCommitsCache
+    }
+
+    const fileInfo = await FileSystem.info(allCommitsPath)
+    if (fileInfo.isFile) {
+        return allCommitsPath
+    } else {
+        console.log(`writing commits to:`, allCommitsPath)
+        await run`git log --first-parent --date=short --pretty=format:%H#%ad ${Cwd(nixpkgsFolder)} ${Stdout(Overwrite(allCommitsPath))}`
+    }
+    const allCommitsAndDatesString = (await FileSystem.read(allCommitsPath)).split("\n")
+    allCommitsCache = Object.fromEntries(allCommitsAndDatesString.map(each=>each.split(/#/)))
+    return allCommitsCache
+}
+
+let allCommitsCache = null
+export async function allCommitsFor({paths}) {
+    const result = await run(`git`, `log`, `--first-parent`, `--date=short`, `--pretty=format:%H#%ad`, ...paths, Cwd(nixpkgsFolder), Stdout(returnAsString))
+    return Object.fromEntries(result.split("\n").map(each=>each.split(/#/)))
+}
+
+export async function getReleventCommitsFor({packageName, startCommit}) {
+    startCommit = startCommit || latestCommitHash
+    const packages = await getPackageInfo({
+        hash: startCommit,
+        packageName
+    })
+    
+    // 
+    // get paths
+    // 
+    const relativePaths = new Set()
+    for (const each of packages) {
+        if (each.meta instanceof Object && typeof each.meta.path == 'string') {
+            relativePaths.add(each.meta.path)
+        }
+    }
+
+    // 
+    // get commits
+    // 
+    return await allCommitsFor(relativePaths)
+}
+
+
+export async function getPackageInfo({hash, packageName}) {
+    const path = `${cacheFolder}/${hash}.json`
+    let output = await jsonRead(path)
+    if (output == null) {
+        // pretend to be linux since it has the most wide support
+        await run`nix-env -qa --json --arg system \"x86_64-linux\" --file ${`https://github.com/NixOS/nixpkgs/archive/${hash}.tar.gz`} ${Stdout(Overwrite(path))}`
+        output = await jsonRead(path)
+    }
+    // if still null
+    if (output == null) {
+        throw Error(`failed to run nix-env -qa for hash:${hash}`)
+    }
+    
+    // filter for package name (sadly this is basically as fast as querying directly for the name)
+    if (packageName) {
+        output = {}
+        for (const [key, value] of Object.entries(output)) {
+            if (value.pname == packageName) {
+                output[key] = value
+            }
+
+            // 
+            // attach package info
+            // 
+            if (value.meta instanceof Object && typeof value.meta.position == 'string') {
+                let relativePath = value.meta.position
+                relativePath = relativePath.replace(/^\/nix\/store\/.+?\//, "")
+                relativePath = relativePath.replace(/^nixpkgs\//, "")
+                relativePath = relativePath.replace(/:\d+$/, "")
+                value.meta.path = relativePath
+            }
+        }
+    }
+
+    return output
+}
