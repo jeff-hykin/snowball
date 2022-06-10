9b6c2a614e1b1c93f37ef6aa23b969adb3fd0e54
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-10 16:11:28 -0500

    2.7.18

--- snowball/snow.nix
+++ snowball/snow.nix
@@ -1,9 +1,9 @@
 {
-        # commit date: 2021-03-19
-        # probably can view at: https://github.com/NixOS/nixpkgs/blob/f5e8bdd07d1afaabf6b37afc5497b1e498b8046f/pkgs/development/interpreters/python/cpython/2.7/default.nix
+        # commit date: 2021-09-15
+        # probably can view at: https://github.com/NixOS/nixpkgs/blob/ff94eef5c14da66ede95e968425afb1eda2c788b/pkgs/development/interpreters/python/cpython/2.7/default.nix
         inputs = {
-            nixpkgsHash   = { ...} : "f5e8bdd07d1afaabf6b37afc5497b1e498b8046f";
-            pkgs          = { nixpkgsHash, ...} : (builtins.import (builtins.fetchTarball ({url="https://github.com/NixOS/nixpkgs/archive/f5e8bdd07d1afaabf6b37afc5497b1e498b8046f.tar.gz";}) ) ({}) );
+            nixpkgsHash   = { ...} : "ff94eef5c14da66ede95e968425afb1eda2c788b";
+            pkgs          = { nixpkgsHash, ...} : (builtins.import (builtins.fetchTarball ({url="https://github.com/NixOS/nixpkgs/archive/ff94eef5c14da66ede95e968425afb1eda2c788b.tar.gz";}) ) ({}) );
             # customInput1 = { pkgs, ...} : pkgs.something;
             # customInput2 = { customInput1, ...} : customInput1.subPackage;
         };
@@ -16,11 +16,11 @@
                     # packages."x86_64-linux" = pkgs.stdenv.mkDerivation {
                     # 
                     # };
-                    packages."x86_64-linux"   = pkgs.python2Full;
-                    packages."aarch64-linux"  = pkgs.python2Full;
-                    packages."i686-linux"     = pkgs.python2Full;
-                    packages."x86_64-darwin"  = pkgs.python2Full;
-                    packages."aarch64-darwin" = pkgs.python2Full;
+                    packages."x86_64-linux"   = pkgs.pythonFull;
+                    packages."aarch64-linux"  = pkgs.pythonFull;
+                    packages."i686-linux"     = pkgs.pythonFull;
+                    packages."x86_64-darwin"  = pkgs.pythonFull;
+                    packages."aarch64-darwin" = pkgs.pythonFull;
                 }
         ;
     }
