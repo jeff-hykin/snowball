5f45d2a30b8547b2ecbb3f7b33dbb9ed24ef753e
Jeff Hykin <jeffhykin@Jeff-MacBookPro.local>
2022-06-10 15:28:40 -0500

    2.7.18

---
+++ snowball/snow.nix
@@ -0,0 +1,25 @@
+{
+        # probably can view at: https://github.com/NixOS/nixpkgs/blob/ff94eef5c14da66ede95e968425afb1eda2c788b/pkgs/development/interpreters/python/cpython/2.7/default.nix
+        inputs = {
+            nixpkgsHash   = { ...} : "ff94eef5c14da66ede95e968425afb1eda2c788b";
+            pkgs          = { nixpkgsHash, ...} : (builtins.import (builtins.fetchTarball ({url="https://github.com/NixOS/nixpkgs/archive/ff94eef5c14da66ede95e968425afb1eda2c788b.tar.gz";}) ) ({}) );
+            # customInput1 = { pkgs, ...} : pkgs.something;
+            # customInput2 = { customInput1, ...} : customInput1.subPackage;
+        };
+        outputs = { pkgs, ... }@input:
+            let
+                exampleValue1 = "blah";
+                exampleValue2 = "blah";
+            in
+                {
+                    # packages."x86_64-linux" = pkgs.stdenv.mkDerivation {
+                    # 
+                    # };
+                    packages."x86_64-linux"   = pkgs.pythonFull;
+                    packages."aarch64-linux"  = pkgs.pythonFull;
+                    packages."i686-linux"     = pkgs.pythonFull;
+                    packages."x86_64-darwin"  = pkgs.pythonFull;
+                    packages."aarch64-darwin" = pkgs.pythonFull;
+                }
+        ;
+    }
