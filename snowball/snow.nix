18b84e0327b76970a3e085ab78a896be7d9fb8f1
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-10 19:25:16 -0500

    3.5.9

--- snowball/snow.nix
+++ snowball/snow.nix
@@ -1,9 +1,9 @@
 {
-        # commit date: 2020-04-05
-        # probably can view at: https://github.com/NixOS/nixpkgs/blob/e50c67ad7eefa8e77436fbd0366b69638b1c8713/pkgs/development/interpreters/python/cpython/default.nix
+        # commit date: 2020-03-29
+        # probably can view at: https://github.com/NixOS/nixpkgs/blob/c392d705181cf3677d1326351bf361354a65e52f/pkgs/development/interpreters/python/cpython/default.nix
         inputs = {
-            nixpkgsHash   = { ...} : "e50c67ad7eefa8e77436fbd0366b69638b1c8713";
-            pkgs          = { nixpkgsHash, ...} : (builtins.import (builtins.fetchTarball ({url="https://github.com/NixOS/nixpkgs/archive/e50c67ad7eefa8e77436fbd0366b69638b1c8713.tar.gz";}) ) ({}) );
+            nixpkgsHash   = { ...} : "c392d705181cf3677d1326351bf361354a65e52f";
+            pkgs          = { nixpkgsHash, ...} : (builtins.import (builtins.fetchTarball ({url="https://github.com/NixOS/nixpkgs/archive/c392d705181cf3677d1326351bf361354a65e52f.tar.gz";}) ) ({}) );
             # customInput1 = { pkgs, ...} : pkgs.something;
             # customInput2 = { customInput1, ...} : customInput1.subPackage;
         };
@@ -16,11 +16,11 @@
                     # packages."x86_64-linux" = pkgs.stdenv.mkDerivation {
                     # 
                     # };
-                    packages."x86_64-linux"   = pkgs.python39;
-                    packages."aarch64-linux"  = pkgs.python39;
-                    packages."i686-linux"     = pkgs.python39;
-                    packages."x86_64-darwin"  = pkgs.python39;
-                    packages."aarch64-darwin" = pkgs.python39;
+                    packages."x86_64-linux"   = pkgs.python35;
+                    packages."aarch64-linux"  = pkgs.python35;
+                    packages."i686-linux"     = pkgs.python35;
+                    packages."x86_64-darwin"  = pkgs.python35;
+                    packages."aarch64-darwin" = pkgs.python35;
                 }
         ;
     }
