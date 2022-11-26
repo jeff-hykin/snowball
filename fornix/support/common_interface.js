e9956083034fe64de4ff1011b0127524e8517fa6
Jeff Hykin <jeff.hykin@gmail.com>
2022-11-26 17:48:58 -0600

    create common interface

---
+++ fornix/support/common_interface.js
@@ -0,0 +1,24 @@
+export let commonInterface = {}
+
+if (globalThis.Deno) {
+    const { FileSystem } = await import(`https://deno.land/x/quickr@0.5.0/main/file_system.js`)
+    const { Console } = await import(`https://deno.land/x/quickr@0.5.0/main/console.js`)
+    const { run, throwIfFails, zipInto, mergeInto, returnAsString, Timeout, Env, Cwd, Stdin, Stdout, Stderr, Out, Overwrite, AppendTo } = await import("https://deno.land/x/quickr@0.5.0/main/run.js")
+    class UserPickedCancel extends Error {}
+    
+    commonInterface = {
+        FileSystem,
+        Console,
+        Custom: {
+            async userAborted() {
+                throw UserPickedCancel()
+            },
+            async getDefaultIdenityName() {
+                try {
+                    return (await run`whoami ${Stdout(returnAsString)}`).replace(/\n/,"").trim() 
+                } catch (error) {}
+                return ""
+            }
+        } 
+    }
+}
