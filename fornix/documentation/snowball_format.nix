021eb1808e7f9ecbd40476677585f27e0c858395
Jeff Hykin <jeff.hykin@gmail.com>
2023-01-30 11:08:24 -0600

    -

--- fornix/documentation/snowball_format.nix
+++ fornix/documentation/snowball_format.nix
@@ -1,31 +1,45 @@
 {
-    # must be JSON-able
-    META = {
-        "isPackage"               = true;
-        "flavor:something: blurb" = true;
-        "commitDate"              = "2022-01-01";
-    };
-    
-    # probably can view at: https://github.com/NixOS/nixpkgs/blob/${nixpkgsHash}/${relativePath}
-    DEFAULT_INPUTS = {
-        _import      = { ... } : url: args: (builtins.import (builtins.fetchTarball ({url=url;}) ) (args) );
-        _nixpkgsHash = { ... } : "3d7144c98c06a4ff4ff39e026bbf7eb4d5403b67";
-        _pkgs        = { _nixpkgsHash, _import, ...} : _import "https://github.com/NixOS/nixpkgs/archive/${nixpkgsHash}.tar.gz" {};
-        python       = { _pkgs, ...} : _pkgs.python3Full;
-        # customInput2 = { customInput1, ...} : customInput1.subPackage;
-    };
-    
-    INPUT_CONSTRAINTS = {
-        _nixpkgsHash = [
-            [ "nixValue:string: a utf-8 encoded string" ]
-            [ "nixValue:commitHash: a 40 character git commit hash" ]
-        ];
-        _pkgs = [
-            [ "flavor:nixpkgs: a bundle of effectively all packages" ]
-        ];
-        python = [
-            [ "flavor:nixpkgs: a bundle of effectively all packages" ]
-        ];
+    INPUT = {
+        # What is expected for each input
+        CONSTRAINTS = {
+            SYSTEM = [
+                # [ "kernel=darwin" "cpu:aarch64" ] # comment out if doesn't work on M1 macs
+                [ "kernel=darwin" "cpu=x86_64" ]
+                [ "kernel=linux" ]
+            ];
+            _nixpkgsHash = [
+                [
+                    "nixValue:string: a utf-8 encoded string"
+                    "nixValue:commitHash: a 40 character git commit hash"
+                ]
+            ];
+            _pkgs = [
+                [ "flavor:nixpkgs: a bundle of effectively all packages" ]
+            ];
+            python = [
+                [
+                    "flavor:python: a programming language that lets you work quickly and integrate systems more effectively"
+                    "version:3"
+                ]
+            ];
+        };
+        # inputs that this was tested with
+        DEFAULT_VALUES = {
+            # SYSTEM is always provided and can't be defaulted here (it will default to the host's system package)
+            SYSTEM       = null;
+            # define a little helper function
+            _import      = { ... } : url: args: (builtins.import (builtins.fetchTarball ({url=url;}) ) (args) );
+            _nixpkgsHash = { ... } : "3d7144c98c06a4ff4ff39e026bbf7eb4d5403b67";
+            _pkgs        = { _nixpkgsHash, _import, ...} : _import "https://github.com/NixOS/nixpkgs/archive/${nixpkgsHash}.tar.gz" {};
+            python       = { _pkgs, ...} : _pkgs.python3Full;
+            # customInput2 = { customInput1, ...} : customInput1.subPackage;
+        };
+        TESTED_VALUES = {
+            _import      = { ... }@args: [ (DEFAULT_VALUES._import      args) ];
+            _nixpkgsHash = { ... }@args: [ (DEFAULT_VALUES._nixpkgsHash args) ];
+            _pkgs        = { ... }@args: [ (DEFAULT_VALUES._pkgs        args) ];
+            python       = { ... }@args: [ (DEFAULT_VALUES.python       args) ];
+        };
     };
     
     PACKAGE_GENERATOR = { python, ... }@input:
@@ -37,11 +51,7 @@
                 # packages."x86_64-linux" = pkgs.stdenv.mkDerivation {
                 # 
                 # };
-                packages."x86_64-linux"   = pkgs.${attributePath};
-                packages."aarch64-linux"  = pkgs.${attributePath};
-                packages."i686-linux"     = pkgs.${attributePath};
-                packages."x86_64-darwin"  = pkgs.${attributePath};
-                packages."aarch64-darwin" = pkgs.${attributePath};
+                packages."${SYSTEM.cpu}-${SYSTEM.kernel}" = python;
             }
     ;
 }
