30019973b8c12b9959209e8901a37bd2c2d5b8e5
Jeff Hykin <jeff.hykin@gmail.com>
2023-03-20 13:26:43 -0500

    change snowball format

--- fornix/documentation/snowball_format.nix
+++ fornix/documentation/snowball_format.nix
@@ -1,48 +1,50 @@
 {
     INPUT = {
-        # What is expected for each input
-        CONSTRAINTS = {
-            SYSTEM = [
+        SYSTEM = {
+            CONSTRAINTS = [
                 # [ "kernel=darwin" "cpu:aarch64" ] # comment out if doesn't work on M1 macs
                 [ "kernel=darwin" "cpu=x86_64" ]
                 [ "kernel=linux" ]
             ];
-            _nixpkgsHash = [
+            GENERATE_DEFAULT_VALUE = null; # system cannot be defaulted (it will always be the current system)
+        };
+        
+        _nixpkgsHash = {
+            CONSTRAINTS = [
+                [ "flavor:nixpkgs: a bundle of effectively all packages" ]
+            ];
+            GENERATE_DEFAULT_VALUE = { ... } : "3d7144c98c06a4ff4ff39e026bbf7eb4d5403b67";
+        };
+        
+        _pkgs = {
+            CONSTRAINTS = [
                 [
                     "nixValue:string: a utf-8 encoded string"
                     "nixValue:commitHash: a 40 character git commit hash"
                 ]
             ];
-            _pkgs = [
-                [ "flavor:nixpkgs: a bundle of effectively all packages" ]
-            ];
-            python = [
+            GENERATE_DEFAULT_VALUE = { _nixpkgsHash, ... }: (url: args: (builtins.import (builtins.fetchTarball ({url=url;}) ) (args) )) "https://github.com/NixOS/nixpkgs/archive/${nixpkgsHash}.tar.gz" {};
+        };
+        
+        python = {
+            CONSTRAINTS = [
                 [
                     "flavor:python: a programming language that lets you work quickly and integrate systems more effectively"
                     "version:3"
                 ]
             ];
-        };
-        # inputs that this was tested with
-        DEFAULT_VALUES = {
-            # SYSTEM is always provided and can't be defaulted here (it will default to the host's system package)
-            SYSTEM       = null;
-            # define a little helper function
-            _import      = { ... } : url: args: (builtins.import (builtins.fetchTarball ({url=url;}) ) (args) );
-            _nixpkgsHash = { ... } : "3d7144c98c06a4ff4ff39e026bbf7eb4d5403b67";
-            _pkgs        = { _nixpkgsHash, _import, ...} : _import "https://github.com/NixOS/nixpkgs/archive/${nixpkgsHash}.tar.gz" {};
-            python       = { _pkgs, ...} : _pkgs.python3Full;
-            # customInput2 = { customInput1, ...} : customInput1.subPackage;
-        };
-        TESTED_VALUES = {
-            _import      = { ... }@args: [ (DEFAULT_VALUES._import      args) ];
-            _nixpkgsHash = { ... }@args: [ (DEFAULT_VALUES._nixpkgsHash args) ];
-            _pkgs        = { ... }@args: [ (DEFAULT_VALUES._pkgs        args) ];
-            python       = { ... }@args: [ (DEFAULT_VALUES.python       args) ];
+            GENERATE_DEFAULT_VALUE = { _pkgs, ...} : _pkgs.python3Full;
         };
     };
     
-    OUPUT = { __magic__, python, ... }@input:
+    TESTED_INPUTS = self: [
+        self {
+            SYSTEM = [ "kernel=darwin" "cpu=x86_64" "os_version=" ]; # system is the only special exception that can be a list of all attributes that the system had
+            _nixpkgsHash = "3d7144c98c06a4ff4ff39e026bbf7eb4d5403b67";
+        }
+    ];
+    
+    OUTPUT = { SYSTEM, python, ... }@input:
         let
             exampleValue1 = "blah";
             exampleValue2 = "blah";
