5b87cd0edb4e620567654a9e45d760ca261196b7
Jeff Hykin <jeff.hykin@gmail.com>
2023-07-30 17:50:36 -0500

    -

--- scrape_versions.ps1
+++ scrape_versions.ps1
@@ -2,8 +2,8 @@
 "\"",`$(echo --% ' |out-null)" >$null;function :{};function dv{<#${/*'>/dev/null )` 2>/dev/null;dv() { #>
 echo "1.33.1"; : --% ' |out-null <#'; }; version="$(dv)"; deno="$HOME/.deno/$version/bin/deno"; if [ -x "$deno" ]; then  exec "$deno" run -q -A --v8-flags=--max-old-space-size=32000 "$0" "$@";  elif [ -f "$deno" ]; then  chmod +x "$deno" && exec "$deno" run -q --v8-flags=--max-old-space-size=32000 -A "$0" "$@";  fi; bin_dir="$HOME/.deno/$version/bin"; exe="$bin_dir/deno"; has () { command -v "$1" >/dev/null; } ;  if ! has unzip; then if ! has apt-get; then  has brew && brew install unzip; else  if [ "$(whoami)" = "root" ]; then  apt-get install unzip -y; elif has sudo; then  echo "Can I install unzip for you? (its required for this command to work) ";read ANSWER;echo;  if [ "$ANSWER" =~ ^[Yy] ]; then  sudo apt-get install unzip -y; fi; elif has doas; then  echo "Can I install unzip for you? (its required for this command to work) ";read ANSWER;echo;  if [ "$ANSWER" =~ ^[Yy] ]; then  doas apt-get install unzip -y; fi; fi;  fi;  fi;  if ! has unzip; then  echo ""; echo "So I couldn't find an 'unzip' command"; echo "And I tried to auto install it, but it seems that failed"; echo "(This script needs unzip and either curl or wget)"; echo "Please install the unzip command manually then re-run this script"; exit 1;  fi;  repo="denoland/deno"; if [ "$OS" = "Windows_NT" ]; then target="x86_64-pc-windows-msvc"; else :;  case $(uname -sm) in "Darwin x86_64") target="x86_64-apple-darwin" ;; "Darwin arm64") target="aarch64-apple-darwin" ;; "Linux aarch64") repo="LukeChannings/deno-arm64" target="linux-arm64" ;; "Linux armhf") echo "deno sadly doesn't support 32-bit ARM. Please check your hardware and possibly install a 64-bit operating system." exit 1 ;; *) target="x86_64-unknown-linux-gnu" ;; esac; fi; deno_uri="https://github.com/$repo/releases/download/v$version/deno-$target.zip"; exe="$bin_dir/deno"; if [ ! -d "$bin_dir" ]; then mkdir -p "$bin_dir"; fi;  if ! curl --fail --location --progress-bar --output "$exe.zip" "$deno_uri"; then if ! wget --output-document="$exe.zip" "$deno_uri"; then echo "Howdy! I looked for the 'curl' and for 'wget' commands but I didn't see either of them. Please install one of them, otherwise I have no way to install the missing deno version needed to run this code"; exit 1; fi; fi; unzip -d "$bin_dir" -o "$exe.zip"; chmod +x "$exe"; rm "$exe.zip"; exec "$deno" run -q --v8-flags=--max-old-space-size=32000 -A "$0" "$@"; #>}; $DenoInstall = "${HOME}/.deno/$(dv)"; $BinDir = "$DenoInstall/bin"; $DenoExe = "$BinDir/deno.exe"; if (-not(Test-Path -Path "$DenoExe" -PathType Leaf)) { $DenoZip = "$BinDir/deno.zip"; $DenoUri = "https://github.com/denoland/deno/releases/download/v$(dv)/deno-x86_64-pc-windows-msvc.zip";  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;  if (!(Test-Path $BinDir)) { New-Item $BinDir -ItemType Directory | Out-Null; };  Function Test-CommandExists { Param ($command); $oldPreference = $ErrorActionPreference; $ErrorActionPreference = "stop"; try {if(Get-Command "$command"){RETURN $true}} Catch {Write-Host "$command does not exist"; RETURN $false}; Finally {$ErrorActionPreference=$oldPreference}; };  if (Test-CommandExists curl) { curl -Lo $DenoZip $DenoUri; } else { curl.exe -Lo $DenoZip $DenoUri; };  if (Test-CommandExists curl) { tar xf $DenoZip -C $BinDir; } else { tar -Lo $DenoZip $DenoUri; };  Remove-Item $DenoZip;  $User = [EnvironmentVariableTarget]::User; $Path = [Environment]::GetEnvironmentVariable('Path', $User); if (!(";$Path;".ToLower() -like "*;$BinDir;*".ToLower())) { [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User); $Env:Path += ";$BinDir"; } }; & "$DenoExe" run -q --v8-flags=--max-old-space-size=32000 -A "$PSCommandPath" @args; Exit $LastExitCode; <# 
 # */0}`;
-import { FileSystem } from "https://deno.land/x/quickr@0.6.36/main/file_system.js"
-import { Console, clearAnsiStylesFrom, black, white, red, green, blue, yellow, cyan, magenta, lightBlack, lightWhite, lightRed, lightGreen, lightBlue, lightYellow, lightMagenta, lightCyan, blackBackground, whiteBackground, redBackground, greenBackground, blueBackground, yellowBackground, magentaBackground, cyanBackground, lightBlackBackground, lightRedBackground, lightGreenBackground, lightYellowBackground, lightBlueBackground, lightMagentaBackground, lightCyanBackground, lightWhiteBackground, bold, reset, dim, italic, underline, inverse, strikethrough, gray, grey, lightGray, lightGrey, grayBackground, greyBackground, lightGrayBackground, lightGreyBackground, } from "https://deno.land/x/quickr@0.6.36/main/console.js"
+import { FileSystem } from "https://deno.land/x/quickr@0.6.38/main/file_system.js"
+import { Console, clearAnsiStylesFrom, black, white, red, green, blue, yellow, cyan, magenta, lightBlack, lightWhite, lightRed, lightGreen, lightBlue, lightYellow, lightMagenta, lightCyan, blackBackground, whiteBackground, redBackground, greenBackground, blueBackground, yellowBackground, magentaBackground, cyanBackground, lightBlackBackground, lightRedBackground, lightGreenBackground, lightYellowBackground, lightBlueBackground, lightMagentaBackground, lightCyanBackground, lightWhiteBackground, bold, reset, dim, italic, underline, inverse, strikethrough, gray, grey, lightGray, lightGrey, grayBackground, greyBackground, lightGrayBackground, lightGreyBackground, } from "https://deno.land/x/quickr@0.6.38/main/console.js"
 import { capitalize, indent, toCamelCase, digitsToEnglishArray, toPascalCase, toKebabCase, toSnakeCase, toScreamingtoKebabCase, toScreamingtoSnakeCase, toRepresentation, toString, regex, escapeRegexMatch, escapeRegexReplace, extractFirst, isValidIdentifier } from "https://deno.land/x/good@1.4.4.1/string.js"
 import { BinaryHeap, ascend, descend } from "https://deno.land/x/good@1.4.4.1/binary_heap.js"
 import { zip } from "https://deno.land/x/good@1.4.4.1/array.js"
@@ -20,7 +20,7 @@ import { debounce } from "https://deno.land/std@0.196.0/async/debounce.ts";
     // then iterate till at least all static-analysis packages have been found
 
 const waitTime = 100 // miliections
-const attributesThatIndicateLeafPackage = ["outPath"] // "has any of these attributes" => children wont be searched
+const attributesThatIndicateLeafPackage = ["outPath", "AAAAAASomeThingsFailToEvaluate"] // "has any of these attributes" => children wont be searched
 let stdoutLogRate = 2000 // every __ miliseconds
 const nodeListOutputPath = "attr_tree.yaml"
 const numberOfParallelNixProcesses = 40
@@ -40,22 +40,20 @@ let numberOfNodes = 0
     await FileSystem.ensureIsFolder(FileSystem.parentPath(nodeListOutputPath))
     const file = await Deno.open(nodeListOutputPath, {read:true, write: true, create: true})
     await file.seek(0, Deno.SeekMode.End)
-    setInterval(() => {
+    setInterval(async () => {
         file.write(new TextEncoder().encode(buffer))
         buffer = ""
     }, stdoutLogRate)
 
 // dumping stdoutLogRate
-    setInterval(async () => {
-        const fileWriteStartTime = (new Date()).getTime()
-        await FileSystem.write({
-            data: JSON.stringify(attrNameCount, 0, 4),
-            path: "attr_name_count.json",
-        }).then(()=>{
-            stdoutLogRate = 2*((new Date()).getTime()-fileWriteStartTime)
-            console.debug(`stdoutLogRate is now:`,stdoutLogRate)
-        })
-    }, 120_000)
+    // setInterval(() => {
+    //     const fileWriteStartTime = (new Date()).getTime()
+    //     FileSystem.sync.write({
+    //         data: JSON.stringify(attrNameCount, 0, 4),
+    //         path: "attr_name_count.json",
+    //     })
+    //     console.debug(`write took:`,((new Date()).getTime()-fileWriteStartTime))
+    // }, 220_000)
 
 
 /**
@@ -268,20 +266,13 @@ async function getAttrNames(attrList, practicalRunNixCommand) {
                 null
             ]
         } catch (error) {
-            throw Error(`
-                getAttrNames failed under the following conditions:
-                    attrList: ${indent({ string:  toRepresentation(attrList), by: "                ", noLead: true })}
-                    command: ${indent({ string:  toRepresentation(command), by: "                ", noLead: true })}
-                    stdout: ${indent({ string:  toRepresentation(stdout), by: "                ", noLead: true })}
-                    stderr: ${indent({ string:  toRepresentation(stderr), by: "                ", noLead: true })}
-            `)
+            throw Error(stderr)
         }
     }
 }
 
 const attrNameCount = {}
 class Node {
-    packageId = undefined
     attrName = ""
     depth = 0
     parent = null
@@ -306,14 +297,13 @@ class Node {
     }
     toJSON() {
         return {
-            packageId: this.packageId,
-            attrName: this.attrName,
             depth: this.depth,
-            hitError: this.hitError,
+            attrName: this.attrName,
             attrPath: this.attrPath,
             isLeaf: this.isLeaf,
             hasVersionAttribute: this.hasVersionAttribute,
             hasNameAttribute: this.hasNameAttribute,
+            hitError: this.hitError,
         }
     }
 }
@@ -345,7 +335,15 @@ async function attrTreeIterator(workers) {
             continue
         }
         // assign some work
+
+        const startTime = (new Date()).getTime()
+        
         const currentNode = branchesToExplore.pop()
+        
+        const endTime = (new Date()).getTime()
+        const duration = endTime - startTime
+        console.log(`pop took ${duration}ms`)
+
         for (const eachWorker of workers) {
             if (!eachWorker.isBusy) {
                 eachWorker.getAttrNames(currentNode.attrPath).then(
@@ -355,40 +353,45 @@ async function attrTreeIterator(workers) {
                         currentNode.isLeaf = names.length == 0
                         currentNode.hasVersionAttribute = names.includes("version")
                         currentNode.hasNameAttribute = names.includes("name")
-                        for (let [attrName, eachSubNames] of zip(names, subNames)) {
-                            eachSubNames = eachSubNames||[]
+                        
+                        const effectivelyLeaf = currentNode.depth != 0 && attributesThatIndicateLeafPackage.some(each=>names.includes(each)) && !names.includes("pkgs")
+                        if (!effectivelyLeaf) {
+                            for (let [attrName, eachSubNames] of zip(names, subNames)) {
+                                eachSubNames = eachSubNames||[]
 
-                            // if we don't have access to the data, then wait for a later iteration
-                            if (!subNamesWasComputed) {
-                                branchesToExplore.push(
-                                    new Node({
-                                        attrName,
-                                        depth: currentNode.depth + 1,
-                                        parent: currentNode,
-                                        isLeaf: null,
-                                        hasVersionAttribute: eachSubNames.includes("version"),
-                                        hasNameAttribute: eachSubNames.includes("name"),
-                                    })
-                                )
-                            // if we were able to grab two levels, go ahead and process it
-                            } else {
-                                buffer += `- { attrName: ${attrName},  depth: ${currentNode.depth + 1}, isLeaf: ${eachSubNames.length == 0}, hasVersionAttribute: ${eachSubNames.includes("version")}, hasNameAttribute: ${eachSubNames.includes("name")}, }\n`
-                                numberOfNodes+=1
-                                // only add to the heap if its not a leaf
-                                if (!eachSubNames.length == 0 && !attributesThatIndicateLeafPackage.some(each=>eachSubNames.includes(each))) {
+                                // if we don't have access to the data, then wait for a later iteration
+                                if (!subNamesWasComputed) {
                                     branchesToExplore.push(
                                         new Node({
                                             attrName,
                                             depth: currentNode.depth + 1,
                                             parent: currentNode,
-                                            isLeaf: eachSubNames.length == 0,
+                                            isLeaf: null,
                                             hasVersionAttribute: eachSubNames.includes("version"),
                                             hasNameAttribute: eachSubNames.includes("name"),
                                         })
                                     )
+                                // if we were able to grab two levels, go ahead and process it
+                                } else {
+                                    buffer += `- { depth: ${currentNode.depth + 1}, attrName: ${attrName}, attrPath: ${currentNode.attrPath.concat([attrName])}, isLeaf: ${eachSubNames.length == 0}, hasVersionAttribute: ${eachSubNames.includes("version")}, hasNameAttribute: ${eachSubNames.includes("name")}, }\n`
+                                    numberOfNodes+=1
+                                    // only add to the heap if its not a leaf
+                                    if (!eachSubNames.length == 0 && !attributesThatIndicateLeafPackage.some(each=>eachSubNames.includes(each))) {
+                                        branchesToExplore.push(
+                                            new Node({
+                                                attrName,
+                                                depth: currentNode.depth + 1,
+                                                parent: currentNode,
+                                                isLeaf: eachSubNames.length == 0,
+                                                hasVersionAttribute: eachSubNames.includes("version"),
+                                                hasNameAttribute: eachSubNames.includes("name"),
+                                            })
+                                        )
+                                    }
                                 }
                             }
                         }
+
                     }
                 ).catch(
                     error=>{
@@ -423,8 +426,13 @@ class Worker {
     async getAttrNames(attrList) {
         this.isBusy = true
         try {
+            const startTime = (new Date()).getTime()
+            
             // console.log(`worker ${this.index} is working on a job`)
             const output = await getAttrNames(attrList, this.practicalRunNixCommand)
+            const endTime = (new Date()).getTime()
+            const duration = endTime - startTime
+            console.log(`worker took ${duration}ms`)
             // console.log(`worker ${this.index} is finished job`)
             return output
         } finally {
@@ -436,5 +444,5 @@ class Worker {
 
 const workers = [...Array(numberOfParallelNixProcesses)].map(each=>new Worker(nixpkgsHash))
 await attrTreeIterator(workers)
-await file.close()
+// await file.close()
 // (this comment is part of deno-guillotine, dont remove) #>
